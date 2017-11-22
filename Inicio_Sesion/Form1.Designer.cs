ce451cd47f1545045b26019941afc7b38477a4ed
Brenda Mejia <33871623+Brenda44@users.noreply.github.com>
2017-11-22 10:03:49 -0600

    Add files via upload

---
+++ Inicio_Sesion/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Inicio_Sesion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.txtUsu = new System.Windows.Forms.TextBox();
+            this.txtCont = new System.Windows.Forms.TextBox();
+            this.btnentrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Inicio_Sesion.Properties.Resources.new_idea;
+            this.pictureBox1.Location = new System.Drawing.Point(248, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Inicio_Sesion.Properties.Resources.user_image_with_black_background__1_;
+            this.pictureBox2.Location = new System.Drawing.Point(218, 195);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Inicio_Sesion.Properties.Resources.padlock__1_;
+            this.pictureBox3.Location = new System.Drawing.Point(218, 254);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // txtUsu
+            // 
+            this.txtUsu.BackColor = System.Drawing.Color.White;
+            this.txtUsu.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsu.Location = new System.Drawing.Point(269, 197);
+            this.txtUsu.Name = "txtUsu";
+            this.txtUsu.Size = new System.Drawing.Size(150, 30);
+            this.txtUsu.TabIndex = 3;
+            // 
+            // txtCont
+            // 
+            this.txtCont.BackColor = System.Drawing.Color.White;
+            this.txtCont.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCont.Location = new System.Drawing.Point(269, 256);
+            this.txtCont.Name = "txtCont";
+            this.txtCont.Size = new System.Drawing.Size(150, 30);
+            this.txtCont.TabIndex = 4;
+            // 
+            // btnentrar
+            // 
+            this.btnentrar.BackgroundImage = global::Inicio_Sesion.Properties.Resources.chevron_sign_to_right;
+            this.btnentrar.Location = new System.Drawing.Point(290, 329);
+            this.btnentrar.Name = "btnentrar";
+            this.btnentrar.Size = new System.Drawing.Size(67, 67);
+            this.btnentrar.TabIndex = 5;
+            this.btnentrar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Inicio_Sesion.Properties.Resources.fondo;
+            this.ClientSize = new System.Drawing.Size(593, 408);
+            this.Controls.Add(this.btnentrar);
+            this.Controls.Add(this.txtCont);
+            this.Controls.Add(this.txtUsu);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Inicio Sesion";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox txtUsu;
+        private System.Windows.Forms.TextBox txtCont;
+        private System.Windows.Forms.Button btnentrar;
+    }
+}
+
